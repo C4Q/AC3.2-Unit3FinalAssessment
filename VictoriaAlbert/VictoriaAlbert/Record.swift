ed559b1242fc584f62bba516ef6472d5faa22d69
Victor Zhong <viczhong@gmail.com>
2016-11-18 12:58:02 -0500

    Refactored to specifications

--- VictoriaAlbert/VictoriaAlbert/Record.swift
+++ VictoriaAlbert/VictoriaAlbert/Record.swift
@@ -9,96 +9,92 @@
 import Foundation
 
 enum RecordModelParseError: Error {
-	case results, fields, object, image, date, place, title
+    case results, fields, object, image, date, place, title
 }
 
 class Record {
-	let object:		String
-	let imageBig:	String //primary_image_id\
-	let imageSmall:	String
-	let date:		String //date_text
-	let place:		String //place
-	let title:		String //title
-	
-	init(object:	String,
-	     imageBig:	String,
-	     imageSmall:String,
-	     date:		String,
-	     place:		String,
-	     title:		String
-		)
-	{
-		self.object = object
-		self.imageBig = imageBig
-		self.imageSmall = imageSmall
-		self.date = date
-		self.place = place
-		self.title = title
-	}
-	
-	convenience init?(from dictionary: [String:AnyObject]) throws {
-		
-		guard let fields = dictionary["fields"] as? [String : AnyObject] else { throw
-			RecordModelParseError.fields }
-		guard let object = fields["object"] as? String else { throw RecordModelParseError.object }
-		guard let image = fields["primary_image_id"] as? String else { throw RecordModelParseError.image }
-		guard image != "" else { return nil }
-		guard let date = fields["date_text"] as? String else { throw RecordModelParseError.date }
-		guard let place = fields["place"] as? String else { throw RecordModelParseError.place }
-		guard let title = fields["title"] as? String else { throw RecordModelParseError.title }
-		
-		func imageToUrl(input: String, big: Bool) -> String {
-			guard input.characters.count > 0 else { return "" }
-			var baseURL = "http://media.vam.ac.uk/media/thira/collection_images/"
-			let charArray = Array(input.characters)
-			
-			for x in 0..<6 {
-				baseURL += String(charArray[x])
-			}
-			
-			if big {
-				baseURL += "/\(input).jpg"
-			}
-			else {
-				baseURL += "/\(input)_jpg_o.jpg"
-			}
-			
-			return baseURL
-		}
-		
-		self.init(object: object,
-		          imageBig: imageToUrl(input: image, big: true),
-		          imageSmall: imageToUrl(input: image, big: false),
-		          date: date,
-		          place: place,
-		          title: title)
-	}
-	
-	static func getRecords(from data: Data) -> [Record]? {
-		var recordsToReturn: [Record]? = []
-		
-		do {
-			let jsonData: Any = try JSONSerialization.jsonObject(with: data, options: [])
-			
-			guard let response: [String : AnyObject] = jsonData as? [String : AnyObject],
-				let records = response["records"] as? [[String: AnyObject]] else {
-					throw RecordModelParseError.results
-			}
-			
-			for recordDict in records {
-				if let record = try Record(from: recordDict) {
-					recordsToReturn?.append(record)
-				}
-			}
-		}
-			
-		catch {
-			print("Parsing Error: \(error)")
-		}
-		
-		return recordsToReturn
-	}
-	
+    let object:		String
+    let imageBig:	String //primary_image_id\
+    let imageSmall:	String
+    let date:		String //date_text
+    let place:		String //place
+    let title:		String //title
+    
+    init(object:	String,
+         imageBig:	String,
+         imageSmall:String,
+         date:		String,
+         place:		String,
+         title:		String
+        )
+    {
+        self.object = object
+        self.imageBig = imageBig
+        self.imageSmall = imageSmall
+        self.date = date
+        self.place = place
+        self.title = title
+    }
+    
+    convenience init?(from dictionary: [String:AnyObject]) throws {
+        guard let fields = dictionary["fields"] as? [String : AnyObject] else { throw
+            RecordModelParseError.fields }
+        guard let object = fields["object"] as? String else { throw RecordModelParseError.object }
+        guard let image = fields["primary_image_id"] as? String else { throw RecordModelParseError.image }
+        guard image != "" else { return nil }
+        guard let date = fields["date_text"] as? String else { throw RecordModelParseError.date }
+        guard let place = fields["place"] as? String else { throw RecordModelParseError.place }
+        guard let title = fields["title"] as? String else { throw RecordModelParseError.title }
+        
+        func imageToUrl(input: String, big: Bool) -> String {
+            guard input.characters.count > 0 else { return "" }
+            var baseURL = "http://media.vam.ac.uk/media/thira/collection_images/"
+            
+            if image.characters.count > 6 {
+                let index = input.index(image.startIndex, offsetBy: 6)
+                baseURL += input.substring(to: index)
+            } else {
+                baseURL += input
+            }
+            
+            if big {
+                baseURL += "/\(input).jpg"
+            } else {
+                baseURL += "/\(input)_jpg_o.jpg"
+            }
+            return baseURL
+        }
+        
+        self.init(object: object,
+                  imageBig: imageToUrl(input: image, big: true),
+                  imageSmall: imageToUrl(input: image, big: false),
+                  date: date,
+                  place: place,
+                  title: title)
+    }
+    
+    static func getRecords(from data: Data) -> [Record]? {
+        var recordsToReturn: [Record]? = []
+        
+        do {
+            let jsonData: Any = try JSONSerialization.jsonObject(with: data, options: [])
+            
+            guard let response: [String : AnyObject] = jsonData as? [String : AnyObject],
+                let records = response["records"] as? [[String: AnyObject]] else {
+                    throw RecordModelParseError.results
+            }
+            
+            for recordDict in records {
+                if let record = try Record(from: recordDict) {
+                    recordsToReturn?.append(record)
+                }
+            }
+        }
+        catch {
+            print("Parsing Error: \(error)")
+        }
+        return recordsToReturn
+    }
 }
 
 
--- VictoriaAlbert/VictoriaAlbert/RecordViewController.swift
+++ VictoriaAlbert/VictoriaAlbert/RecordViewController.swift
@@ -9,31 +9,32 @@
 import UIKit
 
 class RecordViewController: UIViewController {
-	
-	@IBOutlet weak var recordImage: UIImageView!
-	@IBOutlet weak var recordTitle: UILabel!
-	@IBOutlet weak var recordLabel: UILabel!
-	var recordSelected: Record?
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		loadData()
-	}
-	
-	func loadData() {
-		guard let validRecord = recordSelected else { return }
-		
-		APIRequestManager.manager.getData(endPoint: validRecord.imageBig) { (data: Data?) in
-			if  let validData = data,
-				let validImage = UIImage(data: validData) {
-				DispatchQueue.main.async {
-					self.recordImage.image = validImage
-				}
-			}
-		}
-		
-		self.recordTitle.text = validRecord.title
-		self.recordLabel.text = "\(validRecord.object), \(validRecord.date) - \(validRecord.place)"
-	}
+    
+    @IBOutlet weak var recordImage: UIImageView!
+    @IBOutlet weak var recordTitle: UILabel!
+    @IBOutlet weak var recordLabel: UILabel!
+    var recordSelected: Record?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadData()
+    }
+    
+    //MARK: - Load Data
+    
+    func loadData() {
+        guard let validRecord = recordSelected else { return }
+        
+        APIRequestManager.manager.getData(endPoint: validRecord.imageBig) { (data: Data?) in
+            if  let validData = data,
+                let validImage = UIImage(data: validData) {
+                DispatchQueue.main.async {
+                    self.recordImage.image = validImage
+                }
+            }
+        }
+        self.recordTitle.text = validRecord.title
+        self.recordLabel.text = "\(validRecord.object), \(validRecord.date) - \(validRecord.place)"
+    }
 }
 
